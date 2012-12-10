1cde3c3244390c4608f4a9c425c7a7f6feb771ff
RobvanB <RobvanB@gmail.com>
2012-12-09 19:12:56 -0500

    Changed code so class can be called from another class. Need to review and clean up.

--- ruby-lcd.rb
+++ ruby-lcd.rb
@@ -13,301 +13,320 @@
 # 08 Bit 1 (Not used in 4-bit operation)
 # 09 Bit 2 (Not used in 4-bit operation)
 # 10 Bit 3 (Not used in 4-bit operation)
-# 11 (23/4) Bit 4 
+# 11 (23/4) Bit 4
 # 12 (17/0) Bit 5
 # 13 (21/2) Bit 6
 # 14 (22/3) Bit 7
 # 15 Backlight LED Anode (+)
 # 16 Backlight LED Cathode (-)
 
-require 'wiringpi'
-#require File.join(File.dirname(__FILE__), 'parse_bin') #No longer needed, found way easier method to convert to binary
+class RubyLcd
+  require 'wiringpi'
+  #require File.join(File.dirname(__FILE__), 'parse_bin') #No longer needed, found way easier method to convert to binary
 
-@onPi = true # So I can debug the non-RaspberryPi code on a separate machine
+  T_MS = 1.0000000/1000000
+  P_RS = 6
+  P_RW = 99 #Bogus number not used at this moment
+  P_EN = 5
+  P_D0 = 99 #Bogus number not used at this moment
+  P_D1 = 99 #Bogus number not used at this moment
+  P_D2 = 99 #Bogus number not used at this moment
+  P_D3 = 99 #Bogus number not used at this moment
+  P_D4 = 4
+  P_D5 = 0
+  P_D6 = 2
+  P_D7 = 3
+  ON   = 1
+  OFF  = 0
 
-T_MS = 1.0000000/1000000
-P_RS = 6
-P_RW = 99 #Bogus number not used at this moment
-P_EN = 5
-P_D0 = 99 #Bogus number not used at this moment
-P_D1 = 99 #Bogus number not used at this moment
-P_D2 = 99 #Bogus number not used at this moment
-P_D3 = 99 #Bogus number not used at this moment
-P_D4 = 4
-P_D5 = 0
-P_D6 = 2
-P_D7 = 3
-ON   = 1
-OFF  = 0
+  @@charCount = 0
+  @@onPi      = true # So I can debug the non-RaspberryPi code on a separate machine
 
-@@charCount = 0
+  def initialize
+    if (@@onPi == true)
+      Wiringpi.wiringPiSetup
 
-#parseBin = ParseBin.new
+      # Set all pins to output mode (not sure if this is needed)
+      Wiringpi.pinMode(P_RS, 1)
+      Wiringpi.pinMode(P_EN, 1)
+      Wiringpi.pinMode(P_D4, 1)
+      Wiringpi.pinMode(P_D5, 1)
+      Wiringpi.pinMode(P_D6, 1)
+      Wiringpi.pinMode(P_D7, 1)
 
-if (@onPi == true)
-  Wiringpi.wiringPiSetup
-
-  # Set all pins to output mode (not sure if this is needed)
-  Wiringpi.pinMode(P_RS, 1)
-  Wiringpi.pinMode(P_EN, 1)
-  Wiringpi.pinMode(P_D4, 1)
-  Wiringpi.pinMode(P_D5, 1)
-  Wiringpi.pinMode(P_D6, 1)
-  Wiringpi.pinMode(P_D7, 1)
-end
-
-def pulseEnable()
-  # Indicate to LCD that command should be 'executed'
-  Wiringpi.digitalWrite(P_EN, 0)
-  sleep T_MS * 10
-  Wiringpi.digitalWrite(P_EN, 1)
-  sleep T_MS * 10
-  Wiringpi.digitalWrite(P_EN, 0)
-  sleep T_MS * 10
-end
-
-# Turn on display and cursor
-def lcdDisplay(display, cursor, block)
-  Wiringpi.digitalWrite(P_RS, 0)
- #Wiringpi.digitalWrite(P_RW, 0)
-  Wiringpi.digitalWrite(P_D7, 0)
-  Wiringpi.digitalWrite(P_D6, 0)
-  Wiringpi.digitalWrite(P_D5, 0)
-  Wiringpi.digitalWrite(P_D4, 0)
-  pulseEnable()
-
-  Wiringpi.digitalWrite(P_RS, 0)
- #Wiringpi.digitalWrite(P_RW, 0)
-  Wiringpi.digitalWrite(P_D7, 1)
-  Wiringpi.digitalWrite(P_D6, display) # 1 = 0n
-  Wiringpi.digitalWrite(P_D5, cursor) # 1 = Cursor on, 0 = Cursor off
-  Wiringpi.digitalWrite(P_D4, block) # 1 = Block, 0 = Underline cursor
-  pulseEnable()
-end
-
-def setEntryMode()
-  # Entry mode set: move cursor to right after each DD/CGRAM write
-  Wiringpi.digitalWrite(P_RS, 0)
- #Wiringpi.digitalWrite(P_RW, 0)
-  Wiringpi.digitalWrite(P_D7, 0)
-  Wiringpi.digitalWrite(P_D6, 0)
-  Wiringpi.digitalWrite(P_D5, 0)
-  Wiringpi.digitalWrite(P_D4, 0)
-  pulseEnable()
-
-  Wiringpi.digitalWrite(P_RS, 0)
- #Wiringpi.digitalWrite(P_RW, 0)
-  Wiringpi.digitalWrite(P_D7, 0)
-  Wiringpi.digitalWrite(P_D6, 1)
-  Wiringpi.digitalWrite(P_D5, 1)
-  Wiringpi.digitalWrite(P_D4, 0)
-  pulseEnable()
-end
-
-def lcdWrite(charArray)
-  # Write data to CGRAM/DDRAM
-  Wiringpi.digitalWrite(P_RS, 1)
- #Wiringpi.digitalWrite(P_RW, 0)
-  Wiringpi.digitalWrite(P_D7, charArray[0])
-  Wiringpi.digitalWrite(P_D6, charArray[1])
-  Wiringpi.digitalWrite(P_D5, charArray[2])
-  Wiringpi.digitalWrite(P_D4, charArray[3])
-  pulseEnable()
-
-  Wiringpi.digitalWrite(P_RS, 1)
- #Wiringpi.digitalWrite(P_RW, 0)
-  Wiringpi.digitalWrite(P_D7, charArray[4])
-  Wiringpi.digitalWrite(P_D6, charArray[5])
-  Wiringpi.digitalWrite(P_D5, charArray[6])
-  Wiringpi.digitalWrite(P_D4, charArray[7])
-  pulseEnable()
-
-  @@charCount += 1
-end
-
-def cls()
-  # Clear all data from screen
-  Wiringpi.digitalWrite(P_RS, 0)
- #Wiringpi.digitalWrite(P_RW, 0)
-  Wiringpi.digitalWrite(P_D7, 0)
-  Wiringpi.digitalWrite(P_D6, 0)
-  Wiringpi.digitalWrite(P_D5, 0)
-  Wiringpi.digitalWrite(P_D4, 0)
-  pulseEnable()
-
-  Wiringpi.digitalWrite(P_RS, 0)
- #Wiringpi.digitalWrite(P_RW, 0)
-  Wiringpi.digitalWrite(P_D7, 0)
-  Wiringpi.digitalWrite(P_D6, 0)
-  Wiringpi.digitalWrite(P_D5, 0)
-  Wiringpi.digitalWrite(P_D4, 1)
-  pulseEnable()
-end
-
-def initDisplay()
-  # Set function to 4 bit operation
-  i = 0
-  while i < 3     # Needs to be executed 3 times
-    # Wait > 40 MS
-    sleep 42 * T_MS
-    Wiringpi.digitalWrite(P_RS, 0)
-   #Wiringpi.digitalWrite(P_RW, 0)
-    Wiringpi.digitalWrite(P_D7, 0)
-    Wiringpi.digitalWrite(P_D6, 0)
-    Wiringpi.digitalWrite(P_D5, 1)
-    Wiringpi.digitalWrite(P_D4, 1)
-    pulseEnable()
-    i += 1
+      self.class.initDisplay()
+      sleep T_MS * 10
+      self.class.lcdDisplay(ON, ON, OFF)
+      self.class.setEntryMode()
+    end
   end
 
-  # Function set to 4 bit
-  i = 0
-  while i < 2  # Needs to be executed 2 times
+  def self.pulseEnable()
+    # Indicate to LCD that command should be 'executed'
+    Wiringpi.digitalWrite(P_EN, 0)
+    sleep T_MS * 10
+    Wiringpi.digitalWrite(P_EN, 1)
+    sleep T_MS * 10
+    Wiringpi.digitalWrite(P_EN, 0)
+    sleep T_MS * 10
+  end
+
+  # Turn on display and cursor
+  def self.lcdDisplay(display, cursor, block)
     Wiringpi.digitalWrite(P_RS, 0)
    #Wiringpi.digitalWrite(P_RW, 0)
     Wiringpi.digitalWrite(P_D7, 0)
     Wiringpi.digitalWrite(P_D6, 0)
+    Wiringpi.digitalWrite(P_D5, 0)
+    Wiringpi.digitalWrite(P_D4, 0)
+    pulseEnable()
+
+    Wiringpi.digitalWrite(P_RS, 0)
+   #Wiringpi.digitalWrite(P_RW, 0)
+    Wiringpi.digitalWrite(P_D7, 1)
+    Wiringpi.digitalWrite(P_D6, display) # 1 = 0n
+    Wiringpi.digitalWrite(P_D5, cursor) # 1 = Cursor on, 0 = Cursor off
+    Wiringpi.digitalWrite(P_D4, block) # 1 = Block, 0 = Underline cursor
+    pulseEnable()
+  end
+
+  def self.setEntryMode()
+    # Entry mode set: move cursor to right after each DD/CGRAM write
+    Wiringpi.digitalWrite(P_RS, 0)
+   #Wiringpi.digitalWrite(P_RW, 0)
+    Wiringpi.digitalWrite(P_D7, 0)
+    Wiringpi.digitalWrite(P_D6, 0)
+    Wiringpi.digitalWrite(P_D5, 0)
+    Wiringpi.digitalWrite(P_D4, 0)
+    pulseEnable()
+
+    Wiringpi.digitalWrite(P_RS, 0)
+   #Wiringpi.digitalWrite(P_RW, 0)
+    Wiringpi.digitalWrite(P_D7, 0)
+    Wiringpi.digitalWrite(P_D6, 1)
     Wiringpi.digitalWrite(P_D5, 1)
     Wiringpi.digitalWrite(P_D4, 0)
     pulseEnable()
-    i += 1
+    sleep T_MS
   end
 
-  # Set number of display lines
-  Wiringpi.digitalWrite(P_RS, 0)
- #Wiringpi.digitalWrite(P_RW, 0)
-  Wiringpi.digitalWrite(P_D7, 1) #  N = 0 = 1 line display
-  Wiringpi.digitalWrite(P_D6, 0) #  F = 0 = 5x8 character font
-  Wiringpi.digitalWrite(P_D5, 0)
-  Wiringpi.digitalWrite(P_D4, 0)
-  pulseEnable()
+  def self.lcdWrite(charArray)
+    # Write data to CGRAM/DDRAM
+    Wiringpi.digitalWrite(P_RS, 1)
+   #Wiringpi.digitalWrite(P_RW, 0)
+    Wiringpi.digitalWrite(P_D7, charArray[0])
+    Wiringpi.digitalWrite(P_D6, charArray[1])
+    Wiringpi.digitalWrite(P_D5, charArray[2])
+    Wiringpi.digitalWrite(P_D4, charArray[3])
+    pulseEnable()
 
-  sleep T_MS
+    Wiringpi.digitalWrite(P_RS, 1)
+   #Wiringpi.digitalWrite(P_RW, 0)
+    Wiringpi.digitalWrite(P_D7, charArray[4])
+    Wiringpi.digitalWrite(P_D6, charArray[5])
+    Wiringpi.digitalWrite(P_D5, charArray[6])
+    Wiringpi.digitalWrite(P_D4, charArray[7])
+    pulseEnable()
 
-  # Display Off (2 blocks)
-  Wiringpi.digitalWrite(P_RS, 0)
- #Wiringpi.digitalWrite(P_RW, 0)
-  Wiringpi.digitalWrite(P_D7, 0)
-  Wiringpi.digitalWrite(P_D6, 0)
-  Wiringpi.digitalWrite(P_D5, 0)
-  Wiringpi.digitalWrite(P_D4, 0)
-  pulseEnable()
+    @@charCount += 1
+  end
 
-  sleep T_MS
+  def self.cls()
+    # Clear all data from screen
+    Wiringpi.digitalWrite(P_RS, 0)
+   #Wiringpi.digitalWrite(P_RW, 0)
+    Wiringpi.digitalWrite(P_D7, 0)
+    Wiringpi.digitalWrite(P_D6, 0)
+    Wiringpi.digitalWrite(P_D5, 0)
+    Wiringpi.digitalWrite(P_D4, 0)
+    pulseEnable()
 
-  Wiringpi.digitalWrite(P_RS, 0)
- #Wiringpi.digitalWrite(P_RW, 0)
-  Wiringpi.digitalWrite(P_D7, 1)
-  Wiringpi.digitalWrite(P_D6, 0)
-  Wiringpi.digitalWrite(P_D5, 0)
-  Wiringpi.digitalWrite(P_D4, 0)
-  pulseEnable()
+    Wiringpi.digitalWrite(P_RS, 0)
+   #Wiringpi.digitalWrite(P_RW, 0)
+    Wiringpi.digitalWrite(P_D7, 0)
+    Wiringpi.digitalWrite(P_D6, 0)
+    Wiringpi.digitalWrite(P_D5, 0)
+    Wiringpi.digitalWrite(P_D4, 1)
+    pulseEnable()
+  end
 
-  sleep T_MS
-
-  # Display clear (2 blocks)
-  Wiringpi.digitalWrite(P_RS, 0)
- #Wiringpi.digitalWrite(P_RW, 0)
-  Wiringpi.digitalWrite(P_D7, 0)
-  Wiringpi.digitalWrite(P_D6, 0)
-  Wiringpi.digitalWrite(P_D5, 0)
-  Wiringpi.digitalWrite(P_D4, 0)
-  pulseEnable()
-
-  sleep T_MS
-
-  Wiringpi.digitalWrite(P_RS, 0)
- #Wiringpi.digitalWrite(P_RW, 0)
-  Wiringpi.digitalWrite(P_D7, 0)
-  Wiringpi.digitalWrite(P_D6, 0)
-  Wiringpi.digitalWrite(P_D5, 0)
-  Wiringpi.digitalWrite(P_D4, 1)
-  pulseEnable()
-
-  sleep T_MS
-
-  # Entry mode set"
-  Wiringpi.digitalWrite(P_RS, 0)
- #Wiringpi.digitalWrite(P_RW, 0)
-  Wiringpi.digitalWrite(P_D7, 0)
-  Wiringpi.digitalWrite(P_D6, 0)
-  Wiringpi.digitalWrite(P_D5, 0)
-  Wiringpi.digitalWrite(P_D4, 0)
-  pulseEnable()
-
-  sleep T_MS
-
-  Wiringpi.digitalWrite(P_RS, 0)
- #Wiringpi.digitalWrite(P_RW, 0)
-  Wiringpi.digitalWrite(P_D7, 0)
-  Wiringpi.digitalWrite(P_D6, 1)
-  Wiringpi.digitalWrite(P_D5, 1) # 1 = Increment by 1
-  Wiringpi.digitalWrite(P_D4, 1) # 0 = no shift
-  pulseEnable()
-end
-
-def lcdPrint(theText)
-  #Loop through each character in the string, convert it to binary, and print it to the LCD
-  theText.split(//).each { | theChar |
-    puts theChar
-    #binChar = @parseBin.getBin(theChar)
-    binChar = theChar.bytes.first.to_s(2)
-    while binChar.length < 8
-      binChar = "0" + binChar
+  def self.initDisplay()
+    # Set function to 4 bit operation
+    i = 0
+    while i < 3     # Needs to be executed 3 times
+      # Wait > 40 MS
+      sleep 42 * T_MS
+      Wiringpi.digitalWrite(P_RS, 0)
+     #Wiringpi.digitalWrite(P_RW, 0)
+      Wiringpi.digitalWrite(P_D7, 0)
+      Wiringpi.digitalWrite(P_D6, 0)
+      Wiringpi.digitalWrite(P_D5, 1)
+      Wiringpi.digitalWrite(P_D4, 1)
+      pulseEnable()
+      i += 1
     end
 
-    binCharArray = []
-    binChar.split().each {}
-    binChar.split(//).each{ |intChar| binCharArray << intChar.to_i  }
+    # Function set to 4 bit
+    i = 0
+    while i < 2  # Needs to be executed 2 times
+      Wiringpi.digitalWrite(P_RS, 0)
+     #Wiringpi.digitalWrite(P_RW, 0)
+      Wiringpi.digitalWrite(P_D7, 0)
+      Wiringpi.digitalWrite(P_D6, 0)
+      Wiringpi.digitalWrite(P_D5, 1)
+      Wiringpi.digitalWrite(P_D4, 0)
+      pulseEnable()
+      i += 1
+    end
 
-    if (binChar)
-      if (@onPi == true)
-        lcdWrite(binCharArray)
+    # Set number of display lines
+    Wiringpi.digitalWrite(P_RS, 0)
+   #Wiringpi.digitalWrite(P_RW, 0)
+    Wiringpi.digitalWrite(P_D7, 1) #  N = 0 = 1 line display
+    Wiringpi.digitalWrite(P_D6, 0) #  F = 0 = 5x8 character font
+    Wiringpi.digitalWrite(P_D5, 0)
+    Wiringpi.digitalWrite(P_D4, 0)
+    pulseEnable()
+
+    sleep T_MS
+
+    # Display Off (2 blocks)
+    Wiringpi.digitalWrite(P_RS, 0)
+   #Wiringpi.digitalWrite(P_RW, 0)
+    Wiringpi.digitalWrite(P_D7, 0)
+    Wiringpi.digitalWrite(P_D6, 0)
+    Wiringpi.digitalWrite(P_D5, 0)
+    Wiringpi.digitalWrite(P_D4, 0)
+    pulseEnable()
+
+    sleep T_MS
+
+    Wiringpi.digitalWrite(P_RS, 0)
+   #Wiringpi.digitalWrite(P_RW, 0)
+    Wiringpi.digitalWrite(P_D7, 1)
+    Wiringpi.digitalWrite(P_D6, 0)
+    Wiringpi.digitalWrite(P_D5, 0)
+    Wiringpi.digitalWrite(P_D4, 0)
+    pulseEnable()
+
+    sleep T_MS
+
+    # Display clear (2 blocks)
+    Wiringpi.digitalWrite(P_RS, 0)
+   #Wiringpi.digitalWrite(P_RW, 0)
+    Wiringpi.digitalWrite(P_D7, 0)
+    Wiringpi.digitalWrite(P_D6, 0)
+    Wiringpi.digitalWrite(P_D5, 0)
+    Wiringpi.digitalWrite(P_D4, 0)
+    pulseEnable()
+
+    sleep T_MS
+
+    Wiringpi.digitalWrite(P_RS, 0)
+   #Wiringpi.digitalWrite(P_RW, 0)
+    Wiringpi.digitalWrite(P_D7, 0)
+    Wiringpi.digitalWrite(P_D6, 0)
+    Wiringpi.digitalWrite(P_D5, 0)
+    Wiringpi.digitalWrite(P_D4, 1)
+    pulseEnable()
+
+    sleep T_MS
+
+    # Entry mode set"
+    Wiringpi.digitalWrite(P_RS, 0)
+   #Wiringpi.digitalWrite(P_RW, 0)
+    Wiringpi.digitalWrite(P_D7, 0)
+    Wiringpi.digitalWrite(P_D6, 0)
+    Wiringpi.digitalWrite(P_D5, 0)
+    Wiringpi.digitalWrite(P_D4, 0)
+    pulseEnable()
+
+    sleep T_MS
+
+    Wiringpi.digitalWrite(P_RS, 0)
+   #Wiringpi.digitalWrite(P_RW, 0)
+    Wiringpi.digitalWrite(P_D7, 0)
+    Wiringpi.digitalWrite(P_D6, 1)
+    Wiringpi.digitalWrite(P_D5, 1) # 1 = Increment by 1
+    Wiringpi.digitalWrite(P_D4, 1) # 0 = no shift
+    pulseEnable()
+  end
+
+  def self.doLcdPrint(theText)
+    # Loop through each character in the string, convert it to binary, and print it to the LCD
+    theText.split(//).each { | theChar |
+      #puts theChar
+      #binChar = @parseBin.getBin(theChar)
+      binChar = theChar.bytes.first.to_s(2)
+      while binChar.length < 8
+        binChar = "0" + binChar
       end
-      binChar = nil
-    end
-  }
-end
 
-def nextLine()
-  # Display automatically goes to next line when we hit 40 chars
-  fillStr  = " "
-  fillCntr = 1
+      binCharArray = []
+      binChar.split().each {}
+      binChar.split(//).each{ |intChar| binCharArray << intChar.to_i  }
 
-  while (@@charCount + fillCntr < 40)
-    fillStr += " "
-    fillCntr += 1
+      if (binChar)
+        if (@@onPi == true)
+          lcdWrite(binCharArray)
+        end
+        binChar = nil
+      end
+    }
   end
 
-  lcdPrint(fillStr)
+  def self.nextLine()
+    # Display automatically goes to next line when we hit 40 chars
+    fillStr  = " "
+    fillCntr = 1
 
-  @@charCount = 0
+    while (@@charCount + fillCntr < 40)
+      fillStr += " "
+      fillCntr += 1
+    end
+
+    RubyLcd.doLcdPrint(fillStr)
+
+    @@charCount = 0
+  end
+
+  def lcdPrint(theText)
+    # If the string is longer than 16 char - split it.
+    if (theText.length > 16)
+      theText.split(" ").each { | theTextCut |
+        self.class.doLcdPrint(theTextCut)
+        self.class.nextLine()
+        sleep 1
+      }
+    else
+      self.class.doLcdPrint(theText)
+    end
+    self.class.nextLine()
+  end
+
+
+
+
+=begin
+  lcdPrint("Hello Dave.")
+  sleep T_MS
+  nextLine()
+  sleep T_MS
+  lcdPrint("How are things?")
+  sleep 3
+  nextLine()
+  sleep T_MS
+  cls()
+  lcdPrint("Opening the ")
+  sleep T_MS
+  nextLine()
+  lcdPrint("bay doors now...")
+
+  #lcdWrite()
+
+  sleep 10
+  cls()
+=end
+
+
 end
-
-if (@onPi == true)
-  initDisplay()
-  sleep T_MS * 10
-  lcdDisplay(ON, ON, ON)
-  setEntryMode()
-end
-
-
-
-lcdPrint("Hello Dave.")
-sleep T_MS
-nextLine()
-sleep T_MS
-lcdPrint("How are things?")
-sleep 3
-nextLine()
-sleep T_MS
-cls()
-lcdPrint("Opening the ")
-sleep T_MS
-nextLine()
-lcdPrint("bay doors now...")
-
-#lcdWrite()
-
-sleep 10
-cls()
